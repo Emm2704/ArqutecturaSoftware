e7c9ea961ea4673ae7b834e3a6e2a921c1f47f91
Emmanuel Fernando Valencia Sotelo <emmanuelvalencia479@gmail.com.co>
2024-12-11 14:47:15 -0500

    Demo02 ConsoleApp

---
+++ Demo02/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyLibrary;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            string name = "";
+
+            Product Leche;
+            Leche = new Product(1, "Leche entera", 19, 100);
+        }
+    }
+}
